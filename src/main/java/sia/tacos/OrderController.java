ec94e7390a8986b5248e7fa2ae618d7f6ba1b51a
Christian NGAPGUE <christianngapgue@gmail.com>
2023-06-23 14:47:41 +0100

    Apply validation to the processOrder() method

--- src/main/java/sia/tacos/OrderController.java
+++ src/main/java/sia/tacos/OrderController.java
@@ -1,12 +1,14 @@
 package sia.tacos;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,14 +16,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
-    
+
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
 
