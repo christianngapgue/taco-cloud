b3175124623ddfd1653f08a17bcf2a4588dc3983
Christian NGAPGUE <christianngapgue@gmail.com>
2023-06-26 22:54:09 +0100

    Fix a typo in the schema creation script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ create table if not exists Taco_Order (
     delivery_City varchar(50) not null,
     delivery_State varchar(2) not null,
     delivery_Zip varchar(10) not null,
-    cc_eumber varchar(16) not null,
+    cc_number varchar(16) not null,
     cc_expiration varchar(5) not null,
     cc_cvv varchar(3) not null,
     placed_at timestamp not null
