959b1dad6d7fecc717675a6737b2d7db4cb2d11a
Christian NGAPGUE <christianngapgue@gmail.com>
2023-06-28 19:47:15 +0100

    Annotate domain classes with Spring Data

--- src/main/java/sia/tacos/Ingredient.java
+++ src/main/java/sia/tacos/Ingredient.java
@@ -1,15 +1,32 @@
 package sia.tacos;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class Ingredient {
+@Table
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class Ingredient implements Persistable<String> {
 
+    @Id
     private final String id;
     private final String name;
     private final Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+    @Override
+    public boolean isNew() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isNew'");
     }
 }
