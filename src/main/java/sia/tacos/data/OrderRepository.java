4fbd25ac3ea41b042f3bfc7a0cc47736c487a092
Christian NGAPGUE <christianngapgue@gmail.com>
2023-06-28 19:30:09 +0100

    Use Spring Data JDBC's CrudRepository interface
    ...to define data repositories (IngredientRepository and
    OrderRepository).

--- src/main/java/sia/tacos/data/OrderRepository.java
+++ src/main/java/sia/tacos/data/OrderRepository.java
@@ -1,8 +1,8 @@
 package sia.tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
+
 import sia.tacos.TacoOrder;
 
-public interface OrderRepository {
-    
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 }
