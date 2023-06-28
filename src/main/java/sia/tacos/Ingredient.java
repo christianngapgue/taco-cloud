c1a31b290e8bd40c09bfde4f1d62f0bf5f309ca0
Christian NGAPGUE <christianngapgue@gmail.com>
2023-06-28 20:10:23 +0100

    Fix runtime bug
    ...removing/commenting troublesome Lombox annotation
    @NoArgsConstructor().

--- src/main/java/sia/tacos/Ingredient.java
+++ src/main/java/sia/tacos/Ingredient.java
@@ -4,15 +4,13 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Table
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+// @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Ingredient implements Persistable<String> {
 
     @Id
